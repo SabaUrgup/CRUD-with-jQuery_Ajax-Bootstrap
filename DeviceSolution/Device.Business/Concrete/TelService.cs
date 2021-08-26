3153f1744aa66743d85fdf3dc8d8c9b3c2372d73
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-26 12:40:46 +0300

    Layers

---
+++ DeviceSolution/Device.Business/Concrete/TelService.cs
@@ -0,0 +1,86 @@
+﻿using Device.Business.Abstract;
+using Device.DataAccessLayer.Context;
+using Device.DataAccessLayer.DataTransferObject.TelDTO;
+using Device.DataAccessLayer.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Device.Business.Concrete
+{
+    public class TelService : ITelService
+    {
+        public DeviceDatabaseContext _deviceDatabaseContext;
+
+        public TelService(DeviceDatabaseContext deviceDatabaseContext)
+        {
+            _deviceDatabaseContext = deviceDatabaseContext;
+        }
+
+        public int AddTel(TelAddDTO tel)
+        {
+            var newTel = new Tel
+            {
+                Brand = tel.Brand,
+                Color = tel.Color,
+                WarrantyStarting = tel.WarrantyStarting
+
+            };
+            _deviceDatabaseContext.Tels.Add(newTel);
+            return _deviceDatabaseContext.SaveChanges();
+        }
+
+        public int DeleteTel(int Id)
+        {
+            var currentTel = _deviceDatabaseContext.Tels
+                .Where(p => !p.IsDeleted && p.Id == Id).FirstOrDefault();
+            if (currentTel == null)
+            {
+                return 0;
+            }
+            currentTel.IsDeleted = true;
+            _deviceDatabaseContext.Tels.Update(currentTel);
+            return _deviceDatabaseContext.SaveChanges();
+        }
+
+        public TelGetDTO GetTelById(int Id)
+        {
+            return _deviceDatabaseContext.Tels
+                .Where(p => !p.IsDeleted && p.Id == Id)
+                .Select(p => new TelGetDTO
+                {
+                    Brand = p.Brand,
+                    Color = p.Color,
+                    WarrantyStarting = p.WarrantyStarting,
+                }).FirstOrDefault();
+        }
+
+        public List<TelListDTO> ListTel()
+        {
+            return _deviceDatabaseContext.Tels.Where(p => !p.IsDeleted)
+                .Select(p => new TelListDTO
+                {
+                    Id = p.Id,
+                    Brand = p.Brand,
+                    Color = p.Color,
+                    WarrantyStarting = p.WarrantyStarting
+
+                }).ToList();
+        }
+
+        public int UpdateTel(TelUpdateDTO tel)
+        {
+            var currentTel = _deviceDatabaseContext.Tels
+                .Where(p => !p.IsDeleted && p.Id == tel.Id).FirstOrDefault();
+            if (currentTel == null)
+            {
+                return 0;
+            }
+            currentTel.Brand = tel.Brand;
+            currentTel.Color = tel.Color;
+            currentTel.WarrantyStarting = tel.WarrantyStarting;
+
+            _deviceDatabaseContext.Tels.Update(currentTel);
+            return _deviceDatabaseContext.SaveChanges();
+        }
+    }
+}
---
+++ DeviceSolution/Device.Business/Concrete/TvService.cs
@@ -0,0 +1,87 @@
+﻿using Device.Business.Abstract;
+using Device.DataAccessLayer.Context;
+using Device.DataAccessLayer.DataTransferObject.TvDTO;
+using Device.DataAccessLayer.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Device.Business.Concrete
+{
+    public class TvService : ITvService
+    {
+        public DeviceDatabaseContext _deviceDatabaseContext;
+
+        public TvService (DeviceDatabaseContext deviceDatabaseContext)
+        {
+            _deviceDatabaseContext = deviceDatabaseContext;
+        }
+
+        public int AddTv(TvAddDTO tv)
+        {
+            var newTv = new Tv
+            {
+                Brand = tv.Brand,
+                Size = tv.Size,
+                WarrantyStarting = tv.WarrantyStarting
+            };
+            _deviceDatabaseContext.Tvs.Add(newTv);
+            return _deviceDatabaseContext.SaveChanges();
+        }
+
+        public int DeleteTv(int Id)
+        {
+            var currentTv = _deviceDatabaseContext.Tvs
+                .Where(p => !p.IsDeleted && p.Id == Id).FirstOrDefault();
+            if(currentTv == null)
+            {
+                return 0;
+            }
+            currentTv.IsDeleted = true;
+            _deviceDatabaseContext.Tvs.Update(currentTv);
+            return _deviceDatabaseContext.SaveChanges();
+        }
+
+        public TvGetDTO GetTvById(int Id)
+        {
+            return _deviceDatabaseContext.Tvs
+                .Where(p => !p.IsDeleted && p.Id == Id)
+                .Select(p => new TvGetDTO
+                {
+                    Brand = p.Brand,
+                    Size = p.Size,
+                    WarrantyStarting = p.WarrantyStarting,
+                }).FirstOrDefault();
+        }
+
+        public List<TvListDTO> ListTv()
+        {
+            return _deviceDatabaseContext.Tvs.Where(p => !p.IsDeleted)
+                .Select(p => new TvListDTO
+                {
+                    Id = p.Id,
+                    Brand = p.Brand,
+                    Size = p.Size,
+                    WarrantyStarting = p.WarrantyStarting
+
+                }).ToList();
+        }
+
+        public int UpdateTv(TvUpdateDTO tv)
+        {
+            var currentTv = _deviceDatabaseContext.Tvs
+                .Where(p => !p.IsDeleted && p.Id == tv.Id).FirstOrDefault();
+            if(currentTv == null)
+            {
+                return 0;
+            }
+            currentTv.Size = tv.Size;
+            currentTv.Brand = tv.Brand;
+            currentTv.WarrantyStarting = tv.WarrantyStarting;
+
+            _deviceDatabaseContext.Tvs.Update(currentTv);
+            return _deviceDatabaseContext.SaveChanges();
+        }
+    }
+}
