dc8ee8ecf76670857e9a8fa7254622efa7eaacdb
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-08-26 12:49:38 +0300

    Layers

---
+++ DeviceSolution/Device.DataAccessLayer/DataTransferObject/TelDTO/TelGetDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Device.DataAccessLayer.DataTransferObject.TelDTO
+{
+    public class TelGetDTO
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public DateTime WarrantyStarting { get; set; }
+    }
+}
---
+++ DeviceSolution/Device.DataAccessLayer/DataTransferObject/TvDTO/TvGetDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Device.DataAccessLayer.DataTransferObject.TvDTO
+{
+    public class TvGetDTO
+    {
+        public string Brand { get; set; }
+        public double Size { get; set; }
+        public DateTime WarrantyStarting { get; set; }
+    }
+}
