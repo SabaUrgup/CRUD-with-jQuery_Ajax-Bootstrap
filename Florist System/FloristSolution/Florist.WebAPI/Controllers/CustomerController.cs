7a851af96fd38573fecb354ccc1c5766174aed5b
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-09-07 15:58:13 +0300

    Swagger & controllers

---
+++ Florist System/FloristSolution/Florist.WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,85 @@
+﻿using Florist.Business.Abstract;
+using Florist.DataAccessLayer.DataTransferObject.CustomerDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Florist.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        [Route("CustomerAdd")]
+        public async Task<ActionResult<string>> AddCustomer(CustomerAddDTO customer)
+        {
+            var result = await _customerService.AddCustomer(customer);
+
+            if (result > 0)
+            {
+                return Ok(new { code = StatusCode(1000), message = "Müşteri ekleme başarılı!", type = "success" });
+            }
+            return BadRequest("Müşteri ekleme başarısız!");
+        }
+
+        [HttpGet]
+        [Route("CustomerList")]
+        public async Task<ActionResult<List<CustomerListDTO>>> ListCustomer()
+        {
+            var customerList = await _customerService.ListCustomer();
+            if (customerList == null)
+            {
+                return BadRequest("Müşteri listesi bulunamadı!");
+            }
+            return Ok(customerList);
+        }
+
+        [HttpGet]
+        [Route("CustomerIdList/{id}")]
+        public async Task<ActionResult<List<CustomerGetDTO>>> GetCustomerById(int Id)
+        {
+            var customerList = await _customerService.GetCustomerById(Id);
+            if (customerList == null)
+            {
+                return BadRequest("Id listesi bulunamadı!");
+            }
+            return Ok(customerList);
+        }
+
+
+        [HttpPut]
+        [Route("CustomerUpdate")]
+        public async Task<ActionResult<string>> UpdateCustomer(CustomerUpdateDTO customer)
+        {
+            var result = await _customerService.UpdateCustomer(customer);
+            if (result > 0)
+            {
+                return Ok(new { code = StatusCode(1000), message = "Müşteri güncelleme başarılı!", type = "success" });
+            }
+            return BadRequest("Müşteri güncelleme başarısız!");
+        }
+
+        [HttpDelete]
+        [Route("CustomerDelete/{id}")]
+        public async Task<ActionResult<string>> DeleteCustomer(int id)
+        {
+            var result = await _customerService.DeleteCustomer(id);
+            if (result > 0)
+            {
+                return Ok(new { code = StatusCode(1000), message = "Müşteri silme başarılı!", type = "success" });
+            }
+            return BadRequest("Müşteri silme başarısız!");
+        }
+
+    }
+}
---
+++ Florist System/FloristSolution/Florist.WebAPI/Controllers/SaleController.cs
@@ -0,0 +1,72 @@
+﻿using Florist.Business.Abstract;
+using Florist.DataAccessLayer.DataTransferObject.SaleDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Florist.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleController : ControllerBase
+    {
+        public ISaleService _saleService;
+        public SaleController(ISaleService saleService)
+        {
+            _saleService = saleService;
+        }
+
+        [HttpPost]
+        [Route("SaleAdd")]
+        public async Task<ActionResult<string>> AddSale(SaleAddDTO sale)
+        {
+            var result = await _saleService.AddSale(sale);
+
+            if (result > 0)
+            {
+                return Ok(new { code = StatusCode(1000), message = "Satış ekleme başarılı!", type = "success" });
+            }
+            return BadRequest("Satış ekleme başarısız!");
+        }
+
+        [HttpGet]
+        [Route("SaleList")]
+        public async Task<ActionResult<List<SaleListDTO>>> ListSale()
+        {
+            var saleList = await _saleService.ListSale();
+            if (saleList == null)
+            {
+                return BadRequest("Satış listesi bulunamadı!");
+            }
+            return Ok(saleList);
+        }
+
+
+        [HttpPut]
+        [Route("SaleUpdate")]
+        public async Task<ActionResult<string>> UpdateSale(SaleUpdateDTO sale)
+        {
+            var result = await _saleService.UpdateSale(sale);
+            if (result > 0)
+            {
+                return Ok(new { code = StatusCode(1000), message = "Satış güncelleme başarılı!", type = "success" });
+            }
+            return BadRequest("Satış güncelleme başarısız!");
+        }
+
+        [HttpDelete]
+        [Route("SaleDelete")]
+        public async Task<ActionResult<string>> DeleteSale(int id)
+        {
+            var result = await _saleService.DeleteSale(id);
+            if (result > 0)
+            {
+                return Ok(new { code = StatusCode(1000), message = "Satış silme başarılı!", type = "success" });
+            }
+            return BadRequest("Satış silme başarısız!");
+        }
+    }
+}
