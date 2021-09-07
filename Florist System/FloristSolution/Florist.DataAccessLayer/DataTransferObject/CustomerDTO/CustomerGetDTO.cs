41163decab721124bbc15ce4a0aeefb6ae51cae8
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-09-07 15:52:07 +0300

    Database Layer

---
+++ Florist System/FloristSolution/Florist.DataAccessLayer/DataTransferObject/CustomerDTO/CustomerGetDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Florist.DataAccessLayer.DataTransferObject.CustomerDTO
+{
+    public class CustomerGetDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int TelNo { get; set; }
+    }
+}
---
+++ Florist System/FloristSolution/Florist.DataAccessLayer/DataTransferObject/FlowerDTO/FlowerGetDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Florist.DataAccessLayer.DataTransferObject.FlowerDTO
+{
+    public class FlowerGetDTO
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Specification { get; set; }
+        public string FlowerCategoryName { get; set; }
+        public int FlowerCategoryId { get; set; }
+    }
+}
